b3d476d6772dd733f92a2b6b608f863a87291f78
JMMatosF <jmatosfernandes@live.com.pt>
2022-04-05 16:28:56 +0100

    .

--- main.py
+++ main.py
@@ -38,8 +38,7 @@ print('Remotes:')
 for remote in repo.remotes:
     print(f'- {remote.name} {remote.url}')
 try:
-    remote = repo.create_remote('origin', url='https://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@github.com/JMMatosF'
-                                              '/Hugo.git')
+    remote = repo.create_remote('origin', url='git@github.com:JMMatosF/Hugo.git')
 except git.exc.GitCommandError as error:
     print(f'Error creating remote: {error}')
 
