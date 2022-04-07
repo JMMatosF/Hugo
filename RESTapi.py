f46716737cf126e71a9f9e1f3d1c07e532b78f00
JMMatosF <jmatosfernandes@live.com.pt>
2022-04-07 14:45:23 +0100

    s

--- RESTapi.py
+++ RESTapi.py
@@ -1,6 +1,23 @@
-from flask import Flask, jsonify, request
+import markdown
+import flask
+from flask import jsonify
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+with open('a.md', 'r') as f:
+    text = f.read()
+    html = markdown.markdown(text)
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify(html)
 
+
+app.run()
+
+
+if __name__ == '__main__':
+    app.run()
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 export FLASK_APP=./RESTapi.py
+# shellcheck disable=SC2039
+# shellcheck disable=SC2046
 source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
+hugo new site quickstart
+cd quickstart
+git init
+git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+echo theme = \"ananke\" >> config.toml
+hugo new posts/my-first-post.md
+hugo server -D
