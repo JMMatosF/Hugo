d2aae7c3c5f1e2e99eb0139a73a4cbb1426d939c
JMMatosF <jmatosfernandes@live.com.pt>
2022-04-28 15:34:50 +0100

    adicionado

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import time
+
 import git
 import shutil
 from flask import Flask
 from git import Repo
 
 repo = Repo('./')
+
 with repo.config_writer() as git_config:
     git_config.set_value('user', 'email', 'jmatosfernandes@live.com.pt')
     git_config.set_value('user', 'name', 'JMMatosF')
@@ -20,96 +23,88 @@ for remote in repo.remotes:
 try:
     remote = repo.create_remote('origin', url='git@github.com:JMMatosF/Hugo.git')
 except git.exc.GitCommandError as error:
-    print(f'Error creating remote: {error}')
-
-i1 = input("tem um site ou nao?: ")
-if i1 == 'sim':
-    nDir = input("nome do site: ")
-    os.chdir(nDir)
-    i2 = input("Pretende criar um novo post?: ")
-    if i2 == 'sim':
-        nficheiro = input("Escolha nome do ficheiro: ")
-        titulo = input("Escolha titulo do post: ")
-
-        path = os.path.join("content", "posts", nficheiro)
-        file = open(path + '.md', 'a+')
-        file.write("---")
-        file.write("\n")
-        file.write("title: " + "\"" + titulo + "\"")
-        file.write("\n")
-        file.write("draft: true")
-        file.write("\n")
-        file.write("---")
-        file.write("\n")
-
-        while True:
-            text = input("Escreva o que desejar:  ")
-            if text == ".":
-                file.close()
-                repo.git.add(nficheiro)
-                repo.git.commit('-m', 'ola')
-                origin = repo.remote(name='origin')
-                origin.push()
-                os.system("hugo")
-                # repo.git.add("main.py")
-                # repo.git.add(filename)
-                # commit = input("Mensagem de commit: ")
-                # repo.git.commit('-m', commit)
-                # origin = repo.remote(name='origin')
-                # origin.push()
-            if text == "delete":
-                file.truncate(0)
-
-            file.write(text)
-            file.write("\n")
-
-    if i2 == 'nao':
-        os.system("hugo")
-
-if i1 == 'nao':
-    a = input("nome do diretorio: ")
-    os.system("hugo new site " + a + " --force")
-    os.chdir(a)
-    sitename = input("Nome do site: ")
-    os.system("git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke")
-    file = open('config.toml', 'a')
-    file.truncate(0)
-    file.write("baseURL = \'https://JMMatosF.github.io/Hugo\'")
-    file.write("\n")
-    file.write("languageCode = \'en-us\'")
-    file.write("\n")
-    file.write("title = " + "\'" + sitename + "\'")
-    file.write("\n")
-    file.write("theme = 'ananke'")
-    file.write("\n")
-    file.write("publishDir = \"docs\"")
-    file.close()
-    os.system("hugo new posts/my-first-post.md")
-    repo.git.add(a)
-    repo.git.commit('-m', 'ola')
-    origin = repo.remote(name='origin')
-    origin.push()
-    # original = r'Hugo\gh-pages.yml'
-    # target = r'' + a
-    # shutil.copyfile(original, target)
-    os.system("hugo")
+    print('')
 
 
+########################################
 def main():
-    """
-    while True:
-        f = open(filename, "a+", encoding="utf-8")
-        texto = input("Escreva o que desejar: ")
-        if texto == ".":
-            f.close()
-            repo.git.add("main.py")
-            repo.git.add(filename)
-            commit = input("Mensagem de commit: ")
-            repo.git.commit('-m', commit)
-            origin = repo.remote(name='origin')
-            origin.push()
-            exit()
-        f.write(texto)
-        f.write("\n")
-        if texto == "delete":
-            f.truncate(0) """
+    i1 = input("tem um site ou nao?: ")
+    if i1 == 'sim':
+        nDir = input("nome do site: ")
+        os.chdir(nDir)
+        i2 = input("Pretende criar um novo post?: ")
+        if i2 == 'sim':
+            nficheiro = input("Escolha nome do ficheiro: ")
+            titulo = input("Escolha titulo do post: ")
+            path = os.path.join("content", "posts", nficheiro)
+            file = open(path + '.md', 'a+')
+            file.write("---")
+            file.write("\n")
+            file.write("title: " + "\"" + titulo + "\"")
+            file.write("\n")
+            file.write("draft: false")
+            file.write("\n")
+            file.write("---")
+            file.write("\n")
+            while True:
+                text = input("Escreva o que desejar:  ")
+                if text == ".":
+                    file.close()
+                    path = os.path.join("exemple", "content", "posts", nficheiro)
+                    print(os.getcwd())
+                    repo.git.add(path + '.md')
+                    repo.git.add(os.path.join("exemple", "docs"))
+                    repo.git.commit('-m', 'ola')
+                    origin = repo.remote(name='origin')
+                    origin.push()
+                    os.system('xdg-open  http://localhost:1313')
+                    os.system("hugo server -D")
+                    # repo.git.add("main.py")
+                    # repo.git.add(filename)
+                    # commit = input("Mensagem de commit: ")
+                    # repo.git.commit('-m', commit)
+                    # origin = repo.remote(name='origin')
+                    # origin.push()
+
+                if text == "delete":
+                    file.truncate(0)
+
+                file.write(text)
+                file.write("\n")
+
+        if i2 == 'nao':
+            os.system("hugo server -D")
+            time.sleep(2.0)
+            os.system('xdg-open  http://localhost:1313/')
+
+    if i1 == 'nao':
+        a = input("nome do diretorio: ")
+        os.system("hugo new site " + a + " --force")
+        os.chdir(a)
+        sitename = input("Nome do site: ")
+        os.system("git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke")
+        file = open('config.toml', 'a')
+        file.truncate(0)
+        file.write("baseURL = \'http://example.org/\'")
+        file.write("\n")
+        file.write("languageCode = \'en-us\'")
+        file.write("\n")
+        file.write("title = " + "\'" + sitename + "\'")
+        file.write("\n")
+        file.write("theme = 'ananke'")
+        file.write("\n")
+        # file.write("publishDir = \"docs\"")
+        file.close()
+        os.system("hugo new posts/my-first-post.md")
+        # repo.git.add(a)
+        # repo.git.commit('-m', 'ola')
+        # origin = repo.remote(name='origin')
+        # origin.push()
+        # original = r'Hugo\gh-pages.yml'
+        # target = r'' + a
+        # shutil.copyfile(original, target)
+        os.system('xdg-open  http://localhost:1313/')
+        os.system("hugo server -D")
+
+
+main()
